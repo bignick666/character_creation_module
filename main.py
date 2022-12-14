159a30f4d26b6ecc54b1d579679c0d9d1899cdfc
Nikita Berstnev <bignick666@yandex.ru>
2022-12-14 13:54:28 +0300

    My first commit

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def defence(char_name, char_class):
     if char_class == 'warrior':
         return f'{char_name} блокировал {10 + randint(5, 10)} урона'
     if char_class == 'mage':
-        return f'{char_name} блокировал {10 + randint(-2, 2)} урона'
+        return f'{char_name} блокировал {10 + randint(-2, 2)} урона213123123213'
     if char_class == 'healer':
         return f'{char_name} блокировал {10 + randint(2, 5)} урона'
 
